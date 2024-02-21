e12edc534886cc82d79e93f245483283d2137dee
Artur Reiljan <arreil@taltech.ee>
2024-02-21 12:13:47 +0200

    Tere v1

--- EX05/EX05.py
+++ EX05/EX05.py
@@ -2,8 +2,6 @@
 import statistics
 
 import PiBot
-from queue import Queue
-
 
 class Robot:
     """Robot class."""
@@ -13,7 +11,9 @@ class Robot:
         self.front_middle_laser = None
         self.robot = PiBot.PiBot()
         self.shutdown = False
-        self.filter = Queue(maxsize=5)
+        self.filter = []
+        self.laser_reading = None
+
 
     def set_robot(self, robot: PiBot.PiBot()) -> None:
         """Set the PiBot reference."""
@@ -26,19 +26,19 @@ class Robot:
         Returns:
           None if filter is empty, filtered value otherwise.
         """
-        self.filter.put(self.get_front_middle_laser())
-        if self.filter == [] and self.front_middle_laser is None:
+        if not self.filter:
             return None
-        if self.filter.full:
-            self.filter.get()
-        self.filter.put(self.front_middle_laser)
-        return statistics.median(list(self.filter.queue))
-
-
+        else:
+            return median(self.filter)
+        if self.front_middle_laser is not None:
+            self.filter.append(self.laser_reading)
+            if len(self.filter) > 5:
+                self.filter.pop(0)
 
     def sense(self):
         """Sense method as per SPA architecture."""
-        self.front_middle_laser = self.robot.get_front_middle_laser()
+        self.front_middle_laser = self.front_middle_laser
+        self.laser_reading = self.robot.get_front_middle_laser()
 
 
     def spin(self):
