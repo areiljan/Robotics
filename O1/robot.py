e1ffdb4b572080cb2a846996b3d93b69d67e2e6b
Artur Reiljan <arreil@taltech.ee>
2024-03-21 11:28:19 +0200

    Tere v1

--- O1/robot.py
+++ O1/robot.py
@@ -73,7 +73,7 @@ class Robot:
           right 90 degrees is 270 degrees).
         """
         middle_laser = self.get_front_middle_laser()
-        if middle_laser is not None and 0.1 < middle_laser <= 0.45:
+        if middle_laser is not None and round(middle_laser) == 2:
             if self.object_start == 0:
                 self.object_start = self.current_right_encoder
             self.object_end = self.current_right_encoder
