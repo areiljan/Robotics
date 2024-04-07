f3946935107c4edd57e8c8dcd3c30f2d36e66725
Artur Reiljan <arreil@taltech.ee>
2024-04-07 23:53:26 +0300

    Robot Objects Silver 25th push, testing movement.

--- O2/robot.py
+++ O2/robot.py
@@ -132,7 +132,7 @@ class Robot:
                 self.object_start = 0
                 self.object_end = 0
                 self.distance = 0
-        if 0.16 < self.first_object_distance > 0.24:
+        if 0.16 < self.first_object_distance > 0.24 or self.first_object_distance < 0.16:
             self.add = False
 
     def find_objects(self):
