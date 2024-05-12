f045578d6beb5a3224fb5be2fbd30171b5fb4dc4
Artur Reiljan <arreil@taltech.ee>
2024-05-12 22:42:05 +0300

    M2 - Solving the Labyrinth. Using a different approach from the one used in M1.

--- M2/robot.py
+++ M2/robot.py
@@ -1,5 +1,6 @@
-"""Be aMAZE."""
+"""O2 - Objects."""
 
+import math
 import PiBot
 
 
@@ -10,68 +11,154 @@ class Robot:
         """Class initialization."""
         self.robot = PiBot.PiBot()
         self.shutdown = False
-        self.state = "maze"
 
-        self.left_wheel_speed = 13
-        self.right_wheel_speed = 13
-        self.right_acting_speed = 0
-        self.left_acting_speed = 0
+        # Wheel parameters
+        self.wheel_circumference = self.robot.WHEEL_DIAMETER * math.pi
+        self.machine_circumference = self.robot.AXIS_LENGTH * math.pi
 
-        self.right_diagonal_ir = None
-        self.left_diagonal_ir = None
-        self.last_right_diagonal_ir = 0
-        self.last_left_diagonal_ir = 0
-        self.none_seen_ticks = 0
+        # Left wheel settings
+        self.left_wheel_velocity = 8
+        self.left_base_velocity = 0
 
-        # For Calibration
-        self.calibrated = False
-        self.max_right_encoder = 0
-        self.max_left_encoder = 0
-        self.left_factor = 1
-        self.right_factor = 1
+        # Right wheel settings
+        self.right_wheel_velocity = 8
+        self.right_base_velocity = 0
+
+        # Infrared sensor readings
+        self.left_infrared = 0
+        self.right_infrared = 0
+
+        # Rotation settings
+        self.current_rotation = 0
+        self.rotation_setpoint = 0
+        self.has_turned = False
+
+        # Laser sensor reading
+        self.front_laser_reading = 0
+
+        # Other control parameters
+        self.time_driven = 0
+        self.state = "drive"
+
+        # Thresholds and constants
+        self.IR_THRESHOLD = 450
+        self.LASER_THRESHOLD = 0.06
+        self.TIME_TO_TURN = 100
+        self.TIME_TO_FINISH = 40
 
     def set_robot(self, robot: PiBot.PiBot()) -> None:
         """Set robot reference."""
         self.robot = robot
 
-    def drive_in_maze(self):
-        """Stay inbetween walls and stop, when outside the maze."""
-        if self.state == "maze":
-            self.move_backward()
-            print(self.right_diagonal_ir)
-            print(self.left_diagonal_ir)
-            if self.right_diagonal_ir > self.left_diagonal_ir:
-                self.move_backward_right()
-            elif self.right_diagonal_ir < self.left_diagonal_ir:
-                self.move_backward_left()
+    def drive(self):
+        """Drive the robot."""
+        # Check if there's a wall in front and the robot hasn't turned yet
+        wall_in_front = self.front_laser_reading <= self.LASER_THRESHOLD and not self.has_turned
 
-            if (self.last_right_diagonal_ir == self.right_diagonal_ir
-                    and self.last_left_diagonal_ir == self.left_diagonal_ir):
-                self.none_seen_ticks += 1
-                if self.none_seen_ticks == 15:
-                    self.state = "idle"
+        if wall_in_front:
+            # Check if there's no wall on the left side
+            if self.left_infrared < self.IR_THRESHOLD:
+                self.state = "left_turn"
             else:
-                self.none_seen_ticks = 0
+                self.state = "right_turn"
+            self.rotation_setpoint = self.current_rotation
+        elif self.LASER_THRESHOLD < self.front_laser_reading < 2 and self.left_infrared < self.IR_THRESHOLD and not self.has_turned:
+            self.no_wall_turn()
+        else:
+            self.drive_forward()
+
+    def turn_left(self):
+        """Turn left until facing left."""
+        if self.current_rotation < self.rotation_setpoint + 90:
+            self.move_left_on_place()
+        else:
+            self.complete_turn()
+
+    def turn_right(self):
+        """Turn right until facing right."""
+        if self.current_rotation > self.rotation_setpoint - 90:
+            self.move_right_on_place()
+        else:
+            self.complete_turn()
+
+    def complete_turn(self):
+        """Complete turn."""
+        self.has_turned = True  # Robot has turned
+        self.stop()
+        self.state = "drive"
+
+    def drive_forward(self):
+        """Drive forward and check for signs of finish"""
+        turned = self.left_infrared > self.IR_THRESHOLD
+
+        if self.front_laser_reading == 2 and not turned and self.left_infrared < self.IR_THRESHOLD and self.right_infrared < self.IR_THRESHOLD:
+            self.TIME_TO_FINISH -= 1  # a little hardcode
+            if self.TIME_TO_FINISH == 0:
+                self.state = "finish"
+
+        # Move forward
+        self.move_forward()
+
+    def no_wall_turn(self):
+        """Turn when not seeing a wall"""
+        self.time_driven += 1  # Add time
+
+        if self.time_driven == self.TIME_TO_TURN:  # Check the time driven, so robot can start turning if driven enough
+            self.start_turn()
+
+    def start_turn(self):
+        """Start the 90-degree turn."""
+        self.state = "left_turn"
+        self.rotation_setpoint = self.current_rotation  # Capture the current rotation to make 90-degree turn
+        self.time_driven = 0
+
+
+    def finish(self):
+        """Stop the robot, because it has made it to the finish."""
+        self.stop()
+
+    def move_forward(self):
+        """Set robot movement to forward."""
+        self.left_base_velocity = self.left_wheel_velocity
+        self.right_base_velocity = self.right_wheel_velocity
+
+    def move_right_on_place(self):
+        """Set robot movement to right."""
+        self.left_base_velocity = self.left_wheel_velocity
+        self.right_base_velocity = -self.right_wheel_velocity
+
+    def move_left_on_place(self):
+        """Set robot movement to left."""
+        self.left_base_velocity = -self.left_wheel_velocity
+        self.right_base_velocity = self.right_wheel_velocity
+
+    def stop(self):
+        """Set robot movement to halt."""
+        self.left_base_velocity = 0
+        self.right_base_velocity = 0
+
+    def sense(self):
+        """Sense method as per SPA architecture."""
+        self.left_infrared = round(self.robot.get_rear_left_side_ir())
+        self.right_infrared = round(self.robot.get_rear_right_side_ir())
+        self.front_laser_reading = self.robot.get_front_middle_laser()
+        self.current_rotation = self.robot.get_rotation()
 
     def plan(self):
-        """
-        Check for state and activate functions accordingly.
-
-        Use calibrate method for realism.
-        Unknown is the state, where robot should do absolutely nothing.
-        """
-        if self.state == "maze":
-            self.drive_in_maze()
-        elif self.state == "idle":
-            self.stop()
-
-        self.last_right_diagonal_ir = self.right_diagonal_ir
-        self.last_left_diagonal_ir = self.left_diagonal_ir
+        """Plan the robots action."""
+        if self.state == "drive":
+            self.drive()
+        if self.state == "right_turn":
+            self.turn_right()
+        if self.state == "left_turn":
+            self.turn_left()
+        if self.state == "finish":
+            self.finish()
 
     def act(self):
         """Act according to plan."""
-        self.robot.set_left_wheel_speed(self.left_acting_speed * self.left_factor)
-        self.robot.set_right_wheel_speed(self.right_acting_speed * self.right_factor)
+        self.robot.set_right_wheel_speed(self.right_base_velocity)
+        self.robot.set_left_wheel_speed(self.left_base_velocity)
 
     def spin(self):
         """Start the main loop of the robot."""
@@ -81,75 +168,10 @@ class Robot:
             self.act()
             self.robot.sleep(0.05)
 
-    def calibrate(self):
-        """Calibrate the robot."""
-        self.left_wheel_speed = 20
-        self.right_wheel_speed = 20
-        if self.current_rotation < 360 * 3 and not self.calibrated:
-            self.move_left_on_place()
-            self.max_left_encoder = abs(self.current_left_encoder)
-            self.max_right_encoder = abs(self.current_right_encoder)
-        else:
-            self.calibrated = True
-            self.move_right_on_place()
-            if self.max_right_encoder > self.max_left_encoder:
-                self.left_factor = round(1 + (1 - self.max_left_encoder / self.max_right_encoder), 2)
-            elif self.max_right_encoder < self.max_left_encoder:
-                self.right_factor = round(1 + (1 - self.max_right_encoder / self.max_left_encoder), 2)
 
-            self.stop()
-            self.state = "look around"
-            print("Corrections made", self.right_factor, self.left_factor)
-
-    def sense(self):
-        """Sense method as per SPA architecture."""
-        self.right_diagonal_ir = self.robot.get_rear_right_diagonal_ir()
-        self.left_diagonal_ir = self.robot.get_rear_left_diagonal_ir()
-
-    def move_forward(self):
-        """Set robot movement to forward."""
-        self.left_acting_speed = self.left_wheel_speed
-        self.right_acting_speed = self.right_wheel_speed
-
-    def move_backward(self):
-        """Set robot movement to backward."""
-        self.left_acting_speed = -self.left_wheel_speed
-        self.right_acting_speed = -self.right_wheel_speed
-
-    def move_backward_right(self):
-        """Set robot movement to right."""
-        self.left_acting_speed = -self.left_wheel_speed + 3
-        self.right_acting_speed = -self.right_wheel_speed
-
-    def move_backward_left(self):
-        """Set robot movement to left."""
-        self.left_acting_speed = -self.left_wheel_speed
-        self.right_acting_speed = -self.right_wheel_speed + 3
-
-    def move_right_on_place(self):
-        """Set robot movement to right."""
-        self.left_acting_speed = self.left_wheel_speed
-        self.right_acting_speed = -self.right_wheel_speed
-
-    def move_left_on_place(self):
-        """Set robot movement to left."""
-        self.left_acting_speed = -self.left_wheel_speed
-        self.right_acting_speed = self.right_wheel_speed
-
-    def move_right(self):
-        """Set robot movement to right."""
-        self.left_acting_speed = self.left_wheel_speed
-        self.right_acting_speed = -self.right_wheel_speed + 2
-
-    def move_left(self):
-        """Set robot movement to left."""
-        self.left_acting_speed = -self.left_wheel_speed + 2
-        self.right_acting_speed = self.right_wheel_speed
-
-    def stop(self):
-        """Set robot movement to stop."""
-        self.left_acting_speed = 0
-        self.right_acting_speed = 0
+# ------------------------------------------------------------
+# |                          MAIN                            |
+# ------------------------------------------------------------
 
 
 def main():
@@ -160,3 +182,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+robot.py
+8 KB
